bb5bf2565fe803a73e32a700d695299e675418d8
Vladislav Zubakin <shaqimusic@gmail.com>
2022-07-13 03:35:50 +0300

    feat: add range filters UI

--- src/components/product-filters.ts
+++ src/components/product-filters.ts
@@ -1,3 +1,5 @@
+import * as noUiSlider from 'nouislider';
+import 'nouislider/dist/nouislider.css';
 import Stateful from '../models/stateful';
 import { SortFilters, Filters, initialState } from '../utils/filters';
 import products from '../db/products';
@@ -16,6 +18,10 @@ export default class ProductFilters extends Stateful<Filters> {
   private colors: string[];
 
   private capacities: string[];
+
+  private releaseDates: string[];
+
+  private quantities: number[];
 
   public constructor(productList: ProductsList) {
     super(initialState);
@@ -37,6 +43,14 @@ export default class ProductFilters extends Stateful<Filters> {
       if (!a.includes(c.capacity)) a.push(c.capacity);
       return a;
     }, []).sort((a, b) => +b - +a);
+    this.releaseDates = products.reduce((a: string[], c) => {
+      if (!a.includes(c.releaseDate)) a.push(c.releaseDate);
+      return a;
+    }, []).sort((a, b) => Date.parse(a) - Date.parse(b));
+    this.quantities = products.reduce((a: number[], c) => {
+      if (!a.includes(c.inStockCount)) a.push(c.inStockCount);
+      return a;
+    }, []).sort((a, b) => +a - +b);
 
     this.renderContent();
   }
@@ -48,17 +62,70 @@ export default class ProductFilters extends Stateful<Filters> {
       (e: Event) => this.setCategoryFilter(e.target as HTMLLIElement),
     ));
 
+    this.attachSliders();
+
     this.brands.forEach((brand) => (new FilterCheckbox('.brand-filter', brand, 'brand-filter'))
       .element.addEventListener('change', this.setBrandFilter.bind(this)));
     this.colors.forEach((color) => new FilterCheckbox('.color-filter', color, 'color-filter')
       .element.addEventListener('change', this.setColorFilter.bind(this)));
     this.capacities.forEach((capacity) => new FilterCheckbox('.capacity-filter', `${capacity}GB`, 'capacity-filter')
       .element.addEventListener('change', this.setCapacityFilter.bind(this)));
+
     const popular = new FilterCheckbox('.other-filter', 'popular', 'popular-filter');
     popular.element.addEventListener('change', this.setPopularFilter.bind(this));
 
     const sortInput = document.querySelector('#sort-filter') as HTMLSelectElement;
     sortInput.addEventListener('change', (e) => this.setSortFilter(e.target as HTMLSelectElement));
+  }
+
+  private attachSliders() {
+    const quantitySlider = document.getElementById('quantity-filter__slider') as noUiSlider.target;
+    const releaseDateSlider = document.getElementById('release-date-filter__slider') as noUiSlider.target;
+
+    noUiSlider.create(quantitySlider, {
+      start: [
+        this.quantities[0],
+        this.quantities[this.quantities.length - 1],
+      ],
+      connect: true,
+      range: {
+        min: this.quantities[0],
+        max: this.quantities[this.quantities.length - 1],
+      },
+    });
+
+    noUiSlider.create(releaseDateSlider, {
+      start: [
+        Date.parse(this.releaseDates[0]),
+        Date.parse(this.releaseDates[this.releaseDates.length - 1]),
+      ],
+      connect: true,
+      range: {
+        min: Date.parse(this.releaseDates[0]),
+        max: Date.parse(this.releaseDates[this.releaseDates.length - 1]),
+      },
+    });
+
+    const quantityValues = [
+      document.getElementById('quantity-filter__value-min'),
+      document.getElementById('quantity-filter__value-max'),
+    ];
+
+    quantitySlider.noUiSlider?.on('update', (values, handle) => {
+      const element = quantityValues[handle] as HTMLSpanElement;
+      element.innerHTML = `${Math.floor(+values[handle])}`;
+    });
+
+    const releaseDateValues = [
+      document.getElementById('release-date-filter__value-start'),
+      document.getElementById('release-date-filter__value-end'),
+    ];
+
+    releaseDateSlider.noUiSlider?.on('update', (values, handle) => {
+      const element = releaseDateValues[handle] as HTMLSpanElement;
+      const date = new Date(parseInt(`${values[handle]}`, 10));
+      element.innerHTML = `${date.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}`;
+    });
   }
 
   private setPopularFilter(e: Event) {
