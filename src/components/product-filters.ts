340a7df42f060d272bda294a9237d8b4858e04f9
Vladislav Zubakin <shaqimusic@gmail.com>
2022-07-13 03:54:59 +0300

    refactor: remove duplicates, improve readability

--- src/components/product-filters.ts
+++ src/components/product-filters.ts
@@ -33,76 +33,74 @@ export default class ProductFilters extends Stateful<Filters> {
   }
 
   private renderContent() {
-    const categoryList = new CategoryList(this.categories);
-    categoryList.elements.forEach((button) => button.addEventListener(
-      'click',
-      (e: Event) => this.setCategoryFilter(e.target as HTMLLIElement),
-    ));
-
+    this.attachCategoryFilters();
     this.attachRangeFilters();
-
-    this.brands.forEach((brand) => (new FilterCheckbox('.brand-filter', brand, 'brand-filter'))
-      .element.addEventListener('change', this.setBrandFilter.bind(this)));
-    this.colors.forEach((color) => new FilterCheckbox('.color-filter', color, 'color-filter')
-      .element.addEventListener('change', this.setColorFilter.bind(this)));
-    this.capacities.forEach((capacity) => new FilterCheckbox('.capacity-filter', `${capacity}GB`, 'capacity-filter')
-      .element.addEventListener('change', this.setCapacityFilter.bind(this)));
-
-    const popular = new FilterCheckbox('.other-filter', 'popular', 'popular-filter');
-    popular.element.addEventListener('change', this.setPopularFilter.bind(this));
-
-    const sortInput = document.querySelector('#sort-filter') as HTMLSelectElement;
-    sortInput.addEventListener('change', (e) => this.setSortFilter(e.target as HTMLSelectElement));
+    this.attachChekboxFilters();
+    this.attachSortFilters();
   }
 
   private attachRangeFilters() {
     const quantitySlider = document.getElementById('quantity-filter__slider') as noUiSlider.target;
     const releaseDateSlider = document.getElementById('release-date-filter__slider') as noUiSlider.target;
-
-    noUiSlider.create(quantitySlider, {
-      start: [
-        this.quantities[0],
-        this.quantities[this.quantities.length - 1],
-      ],
-      connect: true,
-      range: {
-        min: this.quantities[0],
-        max: this.quantities[this.quantities.length - 1],
-      },
-    });
-
-    noUiSlider.create(releaseDateSlider, {
-      start: [
-        Date.parse(this.releaseDates[0]),
-        Date.parse(this.releaseDates[this.releaseDates.length - 1]),
-      ],
-      connect: true,
-      range: {
-        min: Date.parse(this.releaseDates[0]),
-        max: Date.parse(this.releaseDates[this.releaseDates.length - 1]),
-      },
-    });
-
+    const quantityMin = this.quantities[0];
+    const quantityMax = this.quantities[this.quantities.length - 1];
+    const releaseDateStart = Date.parse(this.releaseDates[0]);
+    const releaseDateEnd = Date.parse(this.releaseDates[this.releaseDates.length - 1]);
     const quantityValues = [
       document.getElementById('quantity-filter__value-min'),
       document.getElementById('quantity-filter__value-max'),
     ];
-
-    quantitySlider.noUiSlider?.on('update', (values, handle) => {
-      const element = quantityValues[handle] as HTMLSpanElement;
-      element.innerHTML = `${Math.floor(+values[handle])}`;
-    });
-
     const releaseDateValues = [
       document.getElementById('release-date-filter__value-start'),
       document.getElementById('release-date-filter__value-end'),
     ];
 
+    noUiSlider.create(quantitySlider, {
+      start: [quantityMin, quantityMax],
+      connect: true,
+      range: { min: quantityMin, max: quantityMax },
+    });
+
+    noUiSlider.create(releaseDateSlider, {
+      start: [releaseDateStart, releaseDateEnd],
+      connect: true,
+      range: { min: releaseDateStart, max: releaseDateEnd },
+    });
+
+    quantitySlider.noUiSlider?.on('update', (values, handle) => {
+      const element = quantityValues[handle] as HTMLSpanElement;
+      element.innerHTML = Math.floor(+values[handle]).toString();
+    });
+
     releaseDateSlider.noUiSlider?.on('update', (values, handle) => {
       const element = releaseDateValues[handle] as HTMLSpanElement;
       const date = new Date(parseInt(`${values[handle]}`, 10));
-      element.innerHTML = `${date.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}`;
+      element.innerHTML = date.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
     });
+  }
+
+  private attachSortFilters() {
+    const sortInput = document.querySelector('#sort-filter') as HTMLSelectElement;
+    sortInput.addEventListener('change', (e) => this.setSortFilter(e.target as HTMLSelectElement));
+  }
+
+  private attachChekboxFilters() {
+    this.brands.forEach((brand) => new FilterCheckbox('.brand-filter', brand, 'brand-filter')
+      .element.addEventListener('change', this.setBrandFilter.bind(this)));
+    this.colors.forEach((color) => new FilterCheckbox('.color-filter', color, 'color-filter')
+      .element.addEventListener('change', this.setColorFilter.bind(this)));
+    this.capacities.forEach((capacity) => new FilterCheckbox('.capacity-filter', `${capacity}GB`, 'capacity-filter')
+      .element.addEventListener('change', this.setCapacityFilter.bind(this)));
+    const popular = new FilterCheckbox('.other-filter', 'popular', 'popular-filter');
+    popular.element.addEventListener('change', this.setPopularFilter.bind(this));
+  }
+
+  private attachCategoryFilters() {
+    const categoryList = new CategoryList(this.categories);
+    categoryList.elements.forEach((button) => button.addEventListener(
+      'click',
+      (e: Event) => this.setCategoryFilter(e.target as HTMLLIElement),
+    ));
   }
 
   private setPopularFilter(e: Event) {
